7717bce4fb4168769e55dcb66d4bb22d0c68777e
jasonerpenbeck <jason.erpenbeck@gmail.com>
2014-09-10 15:06:33 -0700

    funct complete

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,29 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
+  var removed = 0;
+  var added = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[added] = value;
+    size ++;
+    added ++;
   };
 
   someInstance.dequeue = function(){
+    if(size > 0){
+      var removedItem = storage[removed]; //store item removing
+      delete storage[removed]; // remove item
+      removed ++; // increment removed items counter
+      size --; // decrease size counter
+      return removedItem;
+    }
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,26 @@
 var makeStack = function(){
   var someInstance = {};
-
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[size] = value;
+    size ++;
   };
 
   someInstance.pop = function(){
+    if(size > 0){
+      var removedItem = storage[size - 1];
+      delete storage[size - 1];
+      size --;
+      return removedItem;
+    }
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
